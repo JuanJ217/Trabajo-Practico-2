e190c1af546d728d4919c18463d5ae8d503e05ef
JuanJ217 <jjuarez@gi.uba.ar>
2023-11-17 00:56:12 -0300

    3 ideas

--- ideas3.py
+++ ideas3.py
@@ -1,37 +1,19 @@
-from tkinter import Entry, Button, Label, Tk
+import qrcode
+from time import time, localtime, strftime
 
-def ventana_prueba()->None:
 
-    ventana = Tk()
-    ventana.geometry('300x150')
-    ventana.title('')
-    ventana.resizable(width=False, height=False)
+def crear_qr()->None:
+    pelicula = 'Batman-'
+    cine = 'Caballito-'
+    entradas = '5-'
+    tiempo = fecha_y_hora()
+    img = qrcode.make(pelicula + cine + entradas + tiempo)
+    img.save("Prueba 4.png")
 
-    comprar_entradas = Label(ventana, text='SECCION DE RESERVAS')
-    comprar_entradas.pack()
-
-    for opciones in range(1):
-        entrada = Label(ventana, text='Cantidad de entradas')
-        entrada.pack()
-        entrada.place(x=20, y=20)
-
-        opciones_snacks = Button(ventana, text='Añadir snacks')
-        opciones_snacks.pack()
-        opciones_snacks.place(x=60, y=70)
-
-        terminar_compra = Button(ventana, text='Finalizar')
-        terminar_compra.pack()
-        terminar_compra.place(x=150, y=70)
-
-        restar_cantidad = Button(ventana, text='- 1')
-        restar_cantidad.pack()
-
-        sumar_cantidad = Button(ventana, text='+1')
-        sumar_cantidad.pack()
-        sumar_cantidad.place(x=160, y=21)
-
-    ventana.mainloop()
+def fecha_y_hora()->None:
+    estructura = localtime()
+    return strftime('%Y-%m-%d %H:%M:%S', estructura)
 
 def main():
-    ventana_prueba()
+    crear_qr()
 main()
